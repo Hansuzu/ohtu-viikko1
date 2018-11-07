4782849e8722b7c8285015e3e04e7b0cb9a93f23
Hansuzu <hannes.ihalainen@gmail.com>
2018-11-07 14:46:14 +0200

    1 vire korjattu

--- NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -15,47 +15,47 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class StatisticsTest {
-  Player playerKurri=new Player("Kurri",   "EDM", 42, 59);
-  Reader readerStub = new Reader() {
-      public List<Player> getPlayers() {
-          ArrayList<Player> players = new ArrayList<Player>();
+    Player playerKurri=new Player("Kurri",   "EDM", 42, 59);
+    Reader readerStub = new Reader() {
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
 
-          players.add(new Player("Semenko", "EDM", 4, 12));
-          players.add(new Player("Lemieux", "PIT", 45, 54));
-          players.add(playerKurri);
-          players.add(new Player("Yzerman", "DET", 42, 56));
-          players.add(new Player("Gretzky", "EDM", 35, 89));
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(playerKurri);
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
 
-          return players;
-      }
-  };
+            return players;
+        }
+    };
 
-  Statistics stats;
+    Statistics stats;
 
-  @Before
-  public void setUp(){
-      // luodaan Statistics-olio joka käyttää "stubia"
-      stats = new Statistics(readerStub);
-  }  
-  @Test 
-  public void hakuLoytaaPelaajan(){
-    assertEquals(playerKurri, stats.search("Kurri"));
-  }
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }  
+    @Test 
+    public void hakuLoytaaPelaajan(){
+        assertEquals(playerKurri, stats.search("Kurri"));
+    }
 
-  @Test 
-  public void hakuPalauttaaNull(){
-    assertEquals(null, stats.search("Semenkoasg"));
-  }
+    @Test 
+    public void hakuPalauttaaNull(){
+        assertEquals(null, stats.search("Semenkoasg"));
+    }
 
-  @Test 
-  public void tiimiHakuPalauttaaOikeanMaaranPelaajia(){
-    List<Player> res=stats.team("EDM");
-    assertEquals(3, res.size());
-  }
+    @Test 
+    public void tiimiHakuPalauttaaOikeanMaaranPelaajia(){
+        List<Player> res=stats.team("EDM");
+        assertEquals(3, res.size());
+    }
 
-  @Test 
-  public void topScorersHakuPalauttaaOikeanMaaranPelaajia(){
-    List<Player> res=stats.topScorers(1);
-    assertEquals(2, res.size());
-  }
+    @Test 
+    public void topScorersHakuPalauttaaOikeanMaaranPelaajia(){
+        List<Player> res=stats.topScorers(1);
+        assertEquals(2, res.size());
+    }
 }
